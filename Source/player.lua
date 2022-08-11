1c8ef44af205f59f668afc8e9f0e40de0812cec8
JonShort <JonShort@users.noreply.github.com>
2022-08-11 19:56:14 +0100

    [game] - change player to a paddle

--- Source/player.lua
+++ Source/player.lua
@@ -1,11 +1,24 @@
 local gfx <const> = playdate.graphics
 
+local player_width <const> = 60
+local player_height <const> = 10
+
+local function drawPlayer()
+	gfx.setLineWidth(4)
+	gfx.setColor(gfx.kColorBlack)
+	gfx.drawRect(0, 0, player_width, player_height)
+end
+
+local function collisonResponse()
+	return "bounce"
+end
+
 function createPlayer()
-	local playerImage = gfx.image.new("Images/player")
-	assert(playerImage)
-	
-	local player = gfx.sprite.new(playerImage)
-	player:moveTo(200, 230)
+	local player = gfx.sprite.new()
+	player.draw = drawPlayer
+	player.collisonResponse = collisonResponse
+	player:setSize(player_width, player_height)
+	player:moveTo(200, 234)
 	player:add()
 	
 	return player
