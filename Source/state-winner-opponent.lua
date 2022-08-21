6c5073eeb95c633800a1e26dbdc89b8bc3668e56
JonShort <JonShort@users.noreply.github.com>
2022-08-21 15:12:46 +0100

    [game] - improve win/loss screen text

--- Source/state-winner-opponent.lua
+++ Source/state-winner-opponent.lua
@@ -5,7 +5,9 @@ import "stateMachine"
 local gfx <const> = playdate.graphics
 
 function winnerOpponentStateUpdater()
-	gfx.drawTextAligned(string.format("You lose! Press A to play again"), 200, 120, kTextAlignment.center)
+	gfx.drawTextAligned(string.format("You lose!"), 200, 100, kTextAlignment.center)
+
+	gfx.drawTextAligned(string.format("Press A to return to title screen"), 200, 140, kTextAlignment.center)
 
 	if (playdate.buttonJustPressed( playdate.kButtonA )) then
 		updateState("RESTART")
--- Source/state-winner-player.lua
+++ Source/state-winner-player.lua
@@ -5,7 +5,9 @@ import "stateMachine"
 local gfx <const> = playdate.graphics
 
 function winnerPlayerStateUpdater()
-	gfx.drawTextAligned(string.format("You win! Press A to play again"), 200, 120, kTextAlignment.center)
+	gfx.drawTextAligned(string.format("You win!"), 200, 100, kTextAlignment.center)
+
+	gfx.drawTextAligned(string.format("Press A to return to title screen"), 200, 140, kTextAlignment.center)
 
 	if (playdate.buttonJustPressed( playdate.kButtonA )) then
 		updateState("RESTART")
